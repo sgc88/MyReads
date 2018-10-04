c1a73f52adf0f6fb7839776bcba8e40d3351a291
sgc88 <sehribancheng@yahoo.com>
2018-10-03 21:03:50 -0700

    added react router to App.js and created BookSearch and BookShelf components

---
+++ src/BookSearch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BookSearch extends Component {
+  render() {
+    return (
+      <div>BookSearch</div>
+    );
+  }
+}
+
+export default BookSearch;
